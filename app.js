5fe53d868f63a20206751aa26765b4f435cff945
Tabare Sanchez <tabaregabriel@gmail.com>
2025-07-30 14:07:28 -0300

    se mejora la función agregarAmigo para no agregar campos vacios ni repetir nombres

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 let listaAmigos = [];
 
 function agregarAmigo() {
+
+    if (document.getElementById("amigo").value === "") { // a revisar: como tomar los espacios como vacíos
+        alert("El campo no puede estar vacío");
+        return;
+    }
+
+    if (listaAmigos.includes(document.getElementById("amigo").value)) {
+        alert("Ya agregaste a este amigo");
+        return;
+    }
+
     listaAmigos.push(document.getElementById("amigo").value);
     console.log("Amigo: " + listaAmigos[listaAmigos.length - 1] + " agregado a la lista de amigos");
     asignarTextoElemento("#listaAmigos", listaAmigos);
-
 }
 
 function asignarTextoElemento(elemento, lista) {
     let elementoHTML = document.querySelector(elemento);
     let listaAmigos = "";
     for (let i = 0; i < lista.length; i++) {
-        listaAmigos += lista[i]+ "<br>";
+        listaAmigos += lista[i] + "<br>";
     }
     elementoHTML.innerHTML = listaAmigos;
 }
