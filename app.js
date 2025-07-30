5f17188850dcc995e47f4ace6e13578c5ee11a30
Tabare Sanchez <tabaregabriel@gmail.com>
2025-07-29 23:43:45 -0300

    se agregan funcionalidades con placeholder a los botones

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigo() {
+    listaAmigos.push(document.getElementById("amigo").value);
+    alert("amigo :" + listaAmigos[listaAmigos.length - 1] + " agregado");
+
+    mostrarAmigos();
+}
+
+function mostrarAmigos() {
+    for (let i = 0; i < listaAmigos.length; i++) {
+        console.log("Amigo " + (i + 1) + ": " + listaAmigos[i]);
+    }
+}
+function sortearAmigo() {
+    alert("Clic sobre sortear amigo");
+}
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
